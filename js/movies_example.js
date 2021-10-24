96c161be15ce749479bd569344a76daf07980334
Aleksander Petrov <apetrovdev@gmail.com>
2021-10-24 15:07:19 +0300

    adjusted filtering

--- js/movies_example.js
+++ js/movies_example.js
@@ -2,7 +2,10 @@
 const baseUrl = 'https://api.themoviedb.org/3/';
 const apiKey = 'ca3d69ee336e43d8099727f0d7ce3859';
 const imageBaseUrl = 'https://www.themoviedb.org/t/p/w220_and_h330_face';
+const defaultPoster = 'https://www.reelviews.net/resources/img/default_poster.jpg';
 
+let view = 'list';
+let movies = [];
 
 function getMovies(params = {}) {
 
@@ -14,7 +17,8 @@ function getMovies(params = {}) {
         data,
     })
     .done(response => {
-        renderMovieList(response.results);
+        movies = response.results;
+        renderMovieList();
     })
     .fail(response => {
         console.log(response);
@@ -24,7 +28,7 @@ function getMovies(params = {}) {
     })
 }
 
-function renderMovieList(movies = []){
+function renderMovieList(){
     $movieList = $('#movie-list');
     $movieList.empty();
     movies.forEach(movie => {
@@ -34,9 +38,11 @@ function renderMovieList(movies = []){
 }
 
 function getMovieTemplate(movie){
-    const $template = $($('#movie-template').html());
+    const templateSelector = `#movie-${view}-template`;
+    const $template = $($(templateSelector).html());
     $template.find('.movie-title').text(movie.title);
-    $template.find('.movie-poster').attr('src', `${imageBaseUrl}${movie.poster_path}`);
+    const image = movie.poster_path ? `${imageBaseUrl}${movie.poster_path}` : defaultPoster;
+    $template.find('.movie-poster').attr('src', image);
     
     const date = moment(movie.release_date).format('MMM Do YY');
     $template.find('.movie-release-date').text(date);
@@ -45,23 +51,61 @@ function getMovieTemplate(movie){
     return $template;
 }
 
-$('#get-movies').click(()=> {
+function getMovieParams() {
     // Get selected year
-    const year = $('#filter-year').val();
-
+    const timeRange = getDateRangeObject()
     // Get genres from checked checkboxes
     const genres = [];
     $('.genre-checkbox:checked').each((index, el) => {
         genres.push(el.value);
     })
-    const with_genres = genres.join();
+    
+    const sortBy = $('#filter-sort').val();
+    const params = {...timeRange, with_genres: genres.join(), sort_by: sortBy}
+    return params;
+}
 
-    getMovies({
-        year,
-        with_genres,
-    })
+$('#get-movies').click(()=> {
+    getMovies(this.getMovieParams());
 })
 
-getMovies({
-    year: 2021,
+$('#grid-view').click(e => {
+    view = 'grid';
+    $(e.currentTarget).addClass('btn-primary').removeClass('btn-outline-primary');
+    $('#list-view').addClass('btn-outline-primary').removeClass('btn-primary');
+    renderMovieList();
 })
+
+$('#list-view').click(e => {
+    view = 'list';
+    $(e.currentTarget).addClass('btn-primary').removeClass('btn-outline-primary');
+    $('#grid-view').addClass('btn-outline-primary').removeClass('btn-primary');
+    renderMovieList();
+})
+
+function initDatePickers() {
+    const $dateFrom = $("#date-from");
+    $dateFrom.datepicker();
+    const dateFrom = moment().subtract(1, 'years').toDate();
+    $dateFrom.datepicker('setDate', dateFrom);
+
+    const $dateTo = $("#date-to");
+    $dateTo.datepicker();
+    $dateTo.datepicker('setDate', new Date());
+}
+
+function getDateRangeObject() {
+    let dateFrom = $("#date-from").val();
+    let dateTo = $("#date-to").val();
+
+    dateFrom = moment(dateFrom).format('YYYY-MM-DD');
+    dateTo = moment(dateTo).format('YYYY-MM-DD');
+    return {
+        'release_date.gte': dateFrom,
+        'release_date.lte': dateTo,
+    }
+}
+
+initDatePickers();
+getDateRangeObject();
+getMovies(this.getMovieParams());
